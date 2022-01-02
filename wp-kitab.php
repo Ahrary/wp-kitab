163bd9979fa3f36d8e82e5e24dc1027e37510f43
Ahrary <ahraary@gmail.com>
2022-01-02 19:39:30 +0500

    Changed the Domain Path to `languages`

--- wp-kitab.php
+++ wp-kitab.php
@@ -19,7 +19,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-kitab
- * Domain Path:       /languages
+ * Domain Path:       languages
  */
 
 // prevent direct access to the plugin file
